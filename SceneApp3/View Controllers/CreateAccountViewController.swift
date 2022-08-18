4901eafe96c5f9d96940d418cc781ec429611cfc
jlo039 <111025420+jlo039@users.noreply.github.com>
2022-08-17 21:10:46 -0400

    Aug 17 9:10

--- SceneApp3/View Controllers/CreateAccountViewController.swift
+++ SceneApp3/View Controllers/CreateAccountViewController.swift
@@ -8,11 +8,43 @@
 import UIKit
 
 class CreateAccountViewController: UIViewController {
-
+    @IBOutlet weak var typeSelector: UISegmentedControl!
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var cityField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func typeSelectorAction(_ sender: Any) {
+        if typeSelector.selectedSegmentIndex == 0 {
+            print("user")
+        } else if typeSelector.selectedSegmentIndex == 1 {
+            print("artist")
+        } else {
+            print("venue")
+        }
+    }
+    //check to make sure the fields are correct. Returns nil if everything is correct and the error message if not.
+    private func validateFields() -> String? {
+        //check that all fields are filled
+        if firstNameField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || emailField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || usernameField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || passwordField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            cityField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            return "Please fill in all fields."
+        }
+        //check if password is 
+        return nil
+    }
+    @IBAction func CreateAccountAction(_ sender: Any) {
+        //validate fields
+        
+        //create the user
+        
+        //transition to the home screen
     }
     
 

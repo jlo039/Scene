2730c5bc781a9b43806791492a93925b85eaa302
Ethan Lott <ethan.j.lott@gmail.com>
2022-10-19 16:37:49 -0400

    Added programmatic constraint.

--- SceneApp3/View Controllers/AccountTabViewController.swift
+++ SceneApp3/View Controllers/AccountTabViewController.swift
@@ -11,21 +11,29 @@ import FirebaseFirestore
 
 class AccountTabViewController: UIViewController {
 
+    @IBOutlet weak var SafeArea: UIStackView!
+    @IBOutlet weak var TopInfo: UIStackView!
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var displayNameLabel: UILabel!
     @IBOutlet weak var accountTypeLabel: UILabel!
     
+    @IBOutlet weak var InfoTest: UILabel!
     @IBOutlet weak var basicInfoStack: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //var safeX = SafeArea.frame.width
+        //var safeY = SafeArea.frame.height
         
-        basicInfoStack
+        let proPicConstraint = NSLayoutConstraint(item: profileImageView!, attribute: NSLayoutConstraint.Attribute.trailing, relatedBy: NSLayoutConstraint.Relation.equal, toItem: TopInfo, attribute: NSLayoutConstraint.Attribute.trailing, multiplier: 0.25, constant: 0)
+        
+        TopInfo.addConstraint(proPicConstraint)
         
         profileImageView.image = UIImage(named:"AppIcon")
         displayNameLabel.text = Auth.auth().currentUser?.email
         accountTypeLabel.text = "";
         // Do any additional setup after loading the view.
+        
     }
     
 

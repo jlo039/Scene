ed1df15185cd9b2fb0579787cf705f9f94995277
Jason Lott <jason.lott03@gmail.com>
2022-10-26 16:59:29 -0400

    finished UI, still need to fix backend

--- SceneApp3/View Controllers/CreateAccountViewController.swift
+++ SceneApp3/View Controllers/CreateAccountViewController.swift
@@ -10,68 +10,111 @@ import FirebaseAuth
 import FirebaseFirestore
 
 class CreateAccountViewController: UIViewController, UITextFieldDelegate {
-    @IBOutlet weak var typeSelector: UISegmentedControl!
     @IBOutlet weak var firstNameField: UITextField!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var cityField: UITextField!
     @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var stageNameTF: TextField!
+    @IBOutlet weak var venueNameTF: TextField!
+    @IBOutlet weak var addressTF: TextField!
+    
+    var type: Int = 0, firstName: String = "", email: String = "", password: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.firstNameField.delegate = self
-        self.emailField.delegate = self
-        self.usernameField.delegate = self
-        self.passwordField.delegate = self
-        self.cityField.delegate = self
         // Do any additional setup after loading the view.
+        if firstNameField != nil {
+            self.firstNameField.delegate = self
+        }
+        if emailField != nil {
+            self.emailField.delegate = self
+        }
+        if passwordField != nil {
+            self.passwordField.delegate = self
+        }
+        if usernameField != nil {
+            self.usernameField.delegate = self
+        }
+        if stageNameTF != nil {
+            self.stageNameTF.delegate = self
+        }
+        if venueNameTF != nil {
+            self.venueNameTF.delegate = self
+        }
+        if addressTF != nil {
+            self.addressTF.delegate = self
+        }
+        if errorLabel != nil {
+            errorLabel.alpha = 0
+        }
     }
+    
+    @IBAction func memberSelected(_ sender: Any) {
+        type = 0
+    }
+    
+    @IBAction func artistSelected(_ sender: Any) {
+        type = 1
+    }
+    
+    @IBAction func venueSelected(_ sender: Any) {
+        type = 2
+    }
+    
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         switch textField {
         case self.firstNameField:
-            return self.usernameField.becomeFirstResponder()
-        case self.usernameField:
             return self.emailField.becomeFirstResponder()
         case self.emailField:
             return self.passwordField.becomeFirstResponder()
         case self.passwordField:
-            return self.cityField.becomeFirstResponder()
+            nextB(self)
+            return self.passwordField.resignFirstResponder()
+        case self.venueNameTF:
+            return self.addressTF.becomeFirstResponder()
         default:
             CreateAccountAction(self)
-            return self.cityField.resignFirstResponder()
+            return textField.resignFirstResponder()
         }
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-    @IBAction func typeSelectorAction(_ sender: Any) {
-        if typeSelector.selectedSegmentIndex == 0 {
-            
-        } else if typeSelector.selectedSegmentIndex == 1 {
-            
+    
+    @IBAction func nextB(_ sender: Any) {
+        // make sure all fields are filled out
+        let err = validateFields1()
+        if err != nil {
+            showError(err!)
         } else {
-            
+            // get data from text fields and clean
+            firstName = firstNameField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            email = emailField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            password = passwordField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            // change views
+            performSegue(withIdentifier: "nextSegue", sender: nil)
         }
     }
     
-    
     @IBAction func CreateAccountAction(_ sender: Any) {
+        print("account created")
         //validate fields
-        let error = validateFields()
+        var error: String? = ""
+        if usernameField != nil {
+            error = validateFields2()
+        } else if stageNameTF != nil {
+            error = validateFields3()
+        } else {
+            error = validateFields4()
+        }
         if error != nil {
             showError(error!)
         } else {
-            //clean data
-            let email = emailField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-            let password = passwordField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-            let firstName = firstNameField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-            let username = usernameField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-            let city = cityField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-            let type = typeSelector.selectedSegmentIndex
-            //create the user
+            //clean data            //create the user
             Auth.auth().createUser(withEmail: email, password: password) { result, err in
-                
+
                 //check for errors
                 if err != nil {
                     //there was an error
@@ -80,7 +123,7 @@ class CreateAccountViewController: UIViewController, UITextFieldDelegate {
                 } else {
                     //user created sucessfully. store information
                     let db = Firestore.firestore()
-                    
+
                     db.collection("users").addDocument(data: ["firstName" : firstName, "username" : username, "location" : city, "type" : type, "uid" : result!.user.uid]) { error in
                         if error != nil {
                             //show error messsage
@@ -89,7 +132,6 @@ class CreateAccountViewController: UIViewController, UITextFieldDelegate {
                         }
                     }
                     //transition to the home screen
-                    //self.transitionToHome()
                     let storyboard = UIStoryboard(name: "Main", bundle: nil)
                     let mainTabBarController = storyboard.instantiateViewController(withIdentifier: "HomeVC")
                     (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
@@ -101,10 +143,30 @@ class CreateAccountViewController: UIViewController, UITextFieldDelegate {
     }
     
     //check to make sure the fields are correct. Returns nil if everything is correct and the error message if not.
-    func validateFields() -> String? {
+    func validateFields1() -> String? {
         //check that all fields are filled
-        if firstNameField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || emailField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || usernameField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || passwordField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-            cityField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+        if firstNameField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || emailField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""  || passwordField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            return "Please fill in all fields."
+        }
+        return nil
+    }
+    func validateFields2() -> String? {
+        //check that all fields are filled
+        if usernameField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            return "Please fill in all fields."
+        }
+        return nil
+    }
+    func validateFields3() -> String? {
+        //check that all fields are filled
+        if stageNameTF.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            return "Please fill in all fields."
+        }
+        return nil
+    }
+    func validateFields4() -> String? {
+        //check that all fields are filled
+        if venueNameTF.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || addressTF.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
             return "Please fill in all fields."
         }
         return nil

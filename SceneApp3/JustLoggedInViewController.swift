6152c1d0a0e6cd064c1665cb242c9d2e914f4c98
Ethan Lott <ethan.j.lott@gmail.com>
2022-11-16 15:52:49 -0500

    update

--- SceneApp3/JustLoggedInViewController.swift
+++ SceneApp3/JustLoggedInViewController.swift
@@ -15,58 +15,10 @@ class JustLoggedInViewController: UITabBarController {
         
         super.viewDidLoad()
         
-        
-        //let PostTab = viewControllers?[0]
-        let AccountTab = viewControllers?[3] as! AccountTabViewController
-        //let FeedTab = viewControllers?[2]
-        //let ExploreTab = viewControllers?[3]
-        
-        loadAccountScreen(AccountTab: AccountTab)
-        
         // Do any additional setup after loading the view.
          
     }
     
-    func loadAccountScreen(AccountTab: AccountTabViewController) {
-        
-        // Show user's profile picture
-        let proPicConstraint = NSLayoutConstraint(item: AccountTab.profileImageView!, attribute: NSLayoutConstraint.Attribute.trailing, relatedBy: NSLayoutConstraint.Relation.equal, toItem: AccountTab.TopInfo, attribute: NSLayoutConstraint.Attribute.trailing, multiplier: 0.25, constant: 0)
-        AccountTab.TopInfo.addConstraint(proPicConstraint)
-        AccountTab.profileImageView.image = UIImage(named:"AppIcon")
-        
-        // Access Firestore database and current user
-        let db = Firestore.firestore()
-        let signedInUid = Auth.auth().currentUser?.uid
-        
-        // Locate the current user's account info
-        db.collection("users").document(signedInUid!).getDocument { (result, error) in
-            if let document = result, document.exists {
-                AccountTab.displayNameLabel.text = document.get("username") as? String
-                AccountTab.accountTypeLabel.text = "User"
-            } else {
-                db.collection("artist").document(signedInUid!).getDocument { (result, error) in
-                    if let document = result, document.exists {
-                        AccountTab.displayNameLabel.text = document.get("stageName") as? String
-                        AccountTab.accountTypeLabel.text = "Artist"
-                    } else {
-                        db.collection("venue").document(signedInUid!).getDocument { (result, error) in
-                            if let document = result, document.exists {
-                                AccountTab.displayNameLabel.text = document.get("venueName") as? String
-                                AccountTab.accountTypeLabel.text = "Venue"
-                            } else {
-                                print("Document does not exist.")
-                            }
-                        }
-                    }
-                }
-            }
-            AccountTab.displayNameLabel.alpha = 1
-            AccountTab.accountTypeLabel.alpha = 1
-        }
-        
-    }
-    
-
     /*
     // MARK: - Navigation
 

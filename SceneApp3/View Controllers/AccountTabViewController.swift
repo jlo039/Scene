dfaef4541878548f2b0fd609a23158d92fc835f5
jlo039 <111025420+jlo039@users.noreply.github.com>
2022-12-04 21:49:02 -0500

    Fixed

--- SceneApp3/View Controllers/AccountTabViewController.swift
+++ SceneApp3/View Controllers/AccountTabViewController.swift
@@ -6,18 +6,19 @@
 //
 
 import UIKit
+import FirebaseStorage
 import FirebaseAuth
 import FirebaseFirestore
 
-class AccountTabViewController: UIViewController {
+class AccountTabViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var SafeArea: UIStackView!
     @IBOutlet weak var TopInfo: UIStackView!
-    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profilePicIV: UIImageView!
     @IBOutlet weak var displayNameLabel: UILabel!
     @IBOutlet weak var accountTypeLabel: UILabel!
     @IBOutlet weak var realNameLabel: UILabel!
-    
+    @IBOutlet weak var errorL: UILabel!
     @IBOutlet weak var basicInfoStack: UIStackView!
     
     override func viewDidLoad() {
@@ -25,9 +26,34 @@ class AccountTabViewController: UIViewController {
         super.viewDidLoad()
         
         // Define constraint for size of profile picture
-        let proPicConstraint = NSLayoutConstraint(item: profileImageView!, attribute: NSLayoutConstraint.Attribute.trailing, relatedBy: NSLayoutConstraint.Relation.equal, toItem: TopInfo, attribute: NSLayoutConstraint.Attribute.trailing, multiplier: 0.25, constant: 0)
+        let proPicConstraint = NSLayoutConstraint(item: profilePicIV!, attribute: NSLayoutConstraint.Attribute.trailing, relatedBy: NSLayoutConstraint.Relation.equal, toItem: TopInfo, attribute: NSLayoutConstraint.Attribute.trailing, multiplier: 0.25, constant: 0)
         // Apply constraint
         TopInfo.addConstraint(proPicConstraint)
+        
+        // create tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped(gesture:)))
+
+        // add it to the image view;
+        profilePicIV.addGestureRecognizer(tapGesture)
+        // make sure imageView can be interacted with by user
+        profilePicIV.isUserInteractionEnabled = true
+        
+        profilePicIV.layer.borderWidth = 1.0
+        profilePicIV.layer.masksToBounds = false
+        profilePicIV.layer.borderColor = UIColor.white.cgColor
+        profilePicIV.layer.cornerRadius = 44
+        profilePicIV.clipsToBounds = true
+        
+        let task = URLSession.shared.dataTask(with: (Auth.auth().currentUser?.photoURL ?? URL(string: "gs://sceneapp-48eb8.appspot.com/profileImages/chooseProfilePic.jpg"))!, completionHandler: { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                self.profilePicIV.image = image
+            }
+        })
+        task.resume()
         
         // Access Firestore database and current user
         let db = Firestore.firestore()
@@ -44,14 +70,72 @@ class AccountTabViewController: UIViewController {
                 print("Document does not exist")
             }
         }
-        
         accountTypeLabel.text = "Artist"
-        
-        profileImageView.image = UIImage(named:"AppIcon")
-        
     }
     
+    @objc func imageTapped(gesture: UIGestureRecognizer) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        picker.allowsEditing = true
+        present(picker, animated: true)
+    }
 
+    @IBAction func signOutB(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let mainTabBarController = storyboard.instantiateViewController(withIdentifier: "LoginNavigationController")
+            (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
+        } catch let signOutError as NSError {
+            showError(signOutError as! String)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+
+        profilePicIV.image = image
+        guard let imageData = image.pngData() else {
+            return
+        }
+        let signedInUid = Auth.auth().currentUser?.uid
+        let storage = Storage.storage().reference()
+        // upload image data
+        storage.child("profileImages/\(signedInUid ?? "file").png").putData(imageData, metadata: nil, completion: {_, error in
+            guard error == nil else {
+                print("Failed to upload")
+                return
+            }
+            storage.child("profileImages/\(signedInUid ?? "file").png").downloadURL(completion: {url, error in
+                guard let url = url, error == nil else {
+                    return
+                }
+                let urlString = url.absoluteString
+                print("Downlaod url: \(urlString)")
+                let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                changeRequest?.photoURL = url
+                changeRequest?.commitChanges { error in
+                  // ...
+                }
+            })
+        })
+
+
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func showError(_ message:String) {
+        errorL.text = message
+        errorL.alpha = 1
+    }
     
     // MARK: - Navigation
 
